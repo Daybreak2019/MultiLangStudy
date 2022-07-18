8e91657b6aed350c7148530dce58c4f8fd620286
Daybreak2019 <li.wen@wsu.edu>
2022-07-17 22:11:43 -0700

    sniffer by year

--- snifferByYear.sh
+++ snifferByYear.sh
@@ -8,5 +8,5 @@ FileName="ApiSniffer"
 Years=(2010 2011 2012 2013 2014 2015 2016 2017 2018 2019)
 for year in ${Years[@]}
 do
-	python collect.py -s apisniffer -f $FileName &
+	python collect.py -y $year -s apisniffer -f $FileName &
 done
