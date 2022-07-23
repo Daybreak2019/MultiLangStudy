7a9b1db6f5561dd6828a6e0e75f971284b488497
Daybreak2019 <li.wen@wsu.edu>
2022-07-23 07:22:30 -0700

    collect by year

--- collectByYear.sh
+++ collectByYear.sh
@@ -1,7 +1,10 @@
 
-
+startYear=$1
 Years=(2010 2011 2012 2013 2014 2015 2016 2017 2018 2019)
 for year in ${Years[@]}
 do
+	if [ $year -lt $startYear ]; then
+		continue
+	fi
 	python collect.py -y $year -s collect
 done
